d0c95bbe4d877805fd6a630546e43a8f475669c4
kiranbhagannavar <geeky.kiran@gmail.com>
2018-08-25 21:12:03 +0530

    some changes in dbconnection file for cpanel

--- dbconnection.php
+++ dbconnection.php
@@ -1,6 +1,6 @@
 <?php
     $dbconn = mysql_connect('localhost','root','');
-    mysql_select_db('starcarpoint',$dbconn);
+    mysql_select_db('starcarpoint_admin',$dbconn);
     if(mysql_error()){
         die ("Connection to database failed:  ".mysql_error());
     }
