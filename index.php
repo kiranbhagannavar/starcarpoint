d5ba2b4351359c33dd6b95819e59afbdd1148f6d
kiranbhagannavar <geeky.kiran@gmail.com>
2018-09-11 22:48:12 +0530

    index style changes

--- index.php
+++ index.php
@@ -6,96 +6,162 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0" />
 	<title>Welcome To Star Car Point</title>
 	<link rel="shortcut icon" type="image/x-icon" href="css/images/favicon.ico" />
-	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
-	<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all" />
+	<!-- <link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> -->
+	<!-- <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all" /> -->
 	<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700' rel='stylesheet' type='text/css' />
 
-	<script src="js/jquery-1.8.0.min.js" type="text/javascript"></script>
+	<!-- <script src="js/jquery-1.8.0.min.js" type="text/javascript"></script> -->
 	<!--[if lt IE 9]>
 		<script src="js/modernizr.custom.js"></script>
 	<![endif]-->
-	<script src="js/jquery.flexslider-min.js" type="text/javascript"></script>
-	<script src="js/functions.js" type="text/javascript"></script>
+	<!-- <script src="js/jquery.flexslider-min.js" type="text/javascript"></script> -->
+	<!-- <script src="js/functions.js" type="text/javascript"></script> -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<style>
+		:root {
+  --input-padding-x: 1.5rem;
+  --input-padding-y: .75rem;
+}
+
+body {
+  background: #9CECFB;
+  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #0052D4, #65C7F7, #9CECFB);
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #0052D4, #65C7F7, #9CECFB);
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+
+.card-signin {
+  border: 0;
+  border-radius: 1rem;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+}
+
+.card-signin .card-title {
+  margin-bottom: 2rem;
+  font-weight: 300;
+  font-size: 1.5rem;
+}
+
+.card-signin .card-body {
+  padding: 2rem;
+}
+
+.form-signin {
+  width: 100%;
+}
+
+.form-signin .btn {
+  font-size: 80%;
+  border-radius: 5rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+  padding: 1rem;
+  transition: all 0.2s;
+}
+
+.form-label-group {
+  position: relative;
+  margin-bottom: 1rem;
+}
+
+.form-label-group input {
+  border-radius: 2rem;
+}
+
+.form-label-group>input,
+.form-label-group>label {
+  padding: var(--input-padding-y) var(--input-padding-x);
+}
+
+.form-label-group>label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  margin-bottom: 0;
+  /* Override default `<label>` margin */
+  line-height: .8;
+  color: #495057;
+  border: 1px solid transparent;
+  border-radius: .25rem;
+  transition: all .1s ease-in-out;
+}
+
+.form-label-group input::-webkit-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input:-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-moz-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::placeholder {
+  color: transparent;
+}
+
+.form-label-group input:not(:placeholder-shown) {
+  padding-top: calc(var(--input-padding-y) + var(--input-padding-y) * (2 / 3));
+  padding-bottom: calc(var(--input-padding-y) / 3);
+}
+
+.form-label-group input:not(:placeholder-shown)~label {
+  padding-top: calc(var(--input-padding-y) / 3);
+  padding-bottom: calc(var(--input-padding-y) / 3);
+  font-size: 12px;
+  color: #777;
+}
+
+.btn-google {
+  color: white;
+  background-color: #ea4335;
+}
+
+.btn-facebook {
+  color: white;
+  background-color: #3b5998;
+}
+	</style>
 </head>
 
 <body>
-	<!-- wraper -->
-	<div id="wrapper">
-		<!-- shell -->
-		<div class="shell">
-			<!-- container -->
-			<div class="container">
-				<!-- header -->
-				<header id="header">
-					<h1>
-						<a href="index.php">
-							<center>Star Car Point</center>
-						</a>
-					</h1>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+				<div class="card card-signin my-5">
+					<div class="card-body">
+						<h5 class="card-title text-center">Star Car Point</h5>
+						<form class="form-signin" method="post" name="f" action="login.php">
+							<div class="form-label-group">
+								<input type="text" id="inputEmail" name="user3" class="form-control" placeholder="Email address" required
+								 autofocus>
+								<label for="inputEmail">UserName</label>
+							</div>
 
-				</header>
-				<!-- end of header -->
-
-				<!-- main -->
-				<div class="main">
-					<center>
-						<font size="5">Welcome to Star Car Point
-
-							<br />
-							<br />
-						</font>
-					</center>
-
-						<img src="css/images/col-img3.png" alt="" />
-							<h2>Admin Login</h2>
-							<form method="post" name="f" action="login.php">
-								<table>
-									<tr>
-										<td>Username:</td>
-										<td>
-											<input type="text" name="user3" size="15" required/>
-										</td>
-									</tr>
-									<tr>
-										<td>Password:</td>
-										<td>
-											<input type="password" name="pass3" size="15" required/>
-										</td>
-									</tr>
-									<tr>
-										<td></td>
-										<td>
-											<input type="submit" value="Login" />
-										</td>
-									</tr>
-								</table>
-							</form>
-						</div>
-
-					</div>
-
-					<!-- end of main -->
-
-					<div id="footer">
-
-						<div class="footer-bottom">
-
-							<p class="copy">&copy; Copyright 2018 Star Car Point
-								<span>|</span>
-								<strong>Design by
-									<a href="" target="_blank">Kiran Bhagannavar</a>
-								</strong>
-							</p>
-							<div class="cl">&nbsp;</div>
-						</div>
+							<div class="form-label-group">
+								<input type="password" id="inputPassword" name="pass3" class="form-control" placeholder="Password" required>
+								<label for="input	Password">Password</label>
+							</div>
+							<button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+						</form>
 					</div>
 				</div>
-				<!-- end of container -->
 			</div>
-			<!-- end of shell -->
 		</div>
 	</div>
-	<!-- end of wrapper -->
 </body>
-
 </html>
